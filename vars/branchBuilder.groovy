11d8819842c9151515fcf8b3e6e66d880535d80f
andrew morton <drewish@katherinehouse.com>
2017-04-29 14:27:06 -0600

    try fetching

--- vars/branchBuilder.groovy
+++ vars/branchBuilder.groovy
@@ -1,5 +1,9 @@
-import org.kohsuke.github.GitHub
+//import org.kohsuke.github.GitHub
+import java.net.URL
 
 def call(String oauthAccessToken) {
-  def gh = GitHub.connectUsingOAuth(oauthAccessToken)
+//  def gh = GitHub.connectUsingOAuth(oauthAccessToken)
+  echo new URL("http://example.org/").text
+
+  // github_get("https://api.github.com/repos/recurly/#{repo}/issues?per_page=100&state=open&labels=#{URI.escape(label)}")
 }
