d8a86d71e63017327857db844edd341bca5a77a3
andrew morton <drewish@katherinehouse.com>
2017-04-29 23:05:34 -0600

    Try converting

--- vars/branchBuilder.groovy
+++ vars/branchBuilder.groovy
@@ -7,8 +7,10 @@ def call(String oauthAccessToken) {
   def gh = GitHub.connectUsingOAuth(oauthAccessToken)
   // repo:recurly/recurly-app type:pr label:qa4 state:open
   def search = gh.searchIssues().q('repo:recurly/recurly-app').q('type:pr').q('label:qa4').isOpen()
-  def prs = search.list().collect { it.getPullRequest() }
-  echo prs
+  def prs = search.list().collect {
+    it.getRepository().getPullRequest(it.getNumber())
+  }
+  println prs[0]
 
   /// each GHIssue@5152b3be[owner=<null>,assignee=<null>,assignees={},state=open,number=8696,comments=0,labels=[org.kohsuke.github.GHIssue$Label@16bb7ccc],title=Removing unused PAPI code,milestone=<null>,url=https://api.github.com/repos/recurly/recurly-app/issues/8696,id=225163226]
 
