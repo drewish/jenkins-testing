a021bd9648f74aff65b2dccb92d53fbe49e4248e
andrew morton <drewish@katherinehouse.com>
2017-04-29 23:12:01 -0600

    Not sure why issue has no repo

--- vars/branchBuilder.groovy
+++ vars/branchBuilder.groovy
@@ -7,15 +7,16 @@ def call(String oauthAccessToken) {
   def repoName = 'recurly/recurly-app'
   def lableName = 'qa4'
   def gh = GitHub.connectUsingOAuth(oauthAccessToken)
+  def repo = gh.getRepository(repoName)
   // repo:recurly/recurly-app type:pr label:qa4 state:open
 
   def search = gh.searchIssues().q("repo:${repoName}").q('type:pr').q("label:${lableName}").isOpen()
-  def prs = search.list().collect {
+  search.list().collect {
     println it
-    println it.getRepository()
-    it.getRepository().getPullRequest(it.getNumber())
+    println repo
+    repo.getPullRequest(it.getNumber())
   }
-  println prs[0]
+
 
   /// each GHIssue@5152b3be[owner=<null>,assignee=<null>,assignees={},state=open,number=8696,comments=0,labels=[org.kohsuke.github.GHIssue$Label@16bb7ccc],title=Removing unused PAPI code,milestone=<null>,url=https://api.github.com/repos/recurly/recurly-app/issues/8696,id=225163226]
 
