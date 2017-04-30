aeeba163de753e093434aa3e41b43ce160df6fe5
andrew morton <drewish@katherinehouse.com>
2017-04-29 22:53:31 -0600

    try fetching PRs

--- vars/branchBuilder.groovy
+++ vars/branchBuilder.groovy
@@ -5,11 +5,13 @@ import org.kohsuke.github.*
 
 def call(String oauthAccessToken) {
   def gh = GitHub.connectUsingOAuth(oauthAccessToken)
+  // repo:recurly/recurly-app type:pr label:qa4 state:open
   def search = gh.searchIssues().q('repo:recurly/recurly-app').q('type:pr').q('label:qa4').isOpen()
-  search.list().each {
-    echo "each ${it}"
-  }
-//repo:recurly/recurly-app type:pr label:qa4 state:open
+  def prs = search.list().collect { it.getPullRequest() }
+  echo prs
+
+  /// each GHIssue@5152b3be[owner=<null>,assignee=<null>,assignees={},state=open,number=8696,comments=0,labels=[org.kohsuke.github.GHIssue$Label@16bb7ccc],title=Removing unused PAPI code,milestone=<null>,url=https://api.github.com/repos/recurly/recurly-app/issues/8696,id=225163226]
+
 
 //  JsonSlurper
 //  req.add_field "Authorization", "token #{ENV['GITHUB_TOKEN']}"
